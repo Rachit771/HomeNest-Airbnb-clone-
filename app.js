bccc981a3aded29fc603ee505c191b32453f4a0f
Rachit771 <sharmarachit554@gmail.com>
2025-10-16 20:55:49 +0530

    Change authentication from cookies to session

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const path = require('path');
 // External Module
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 //Local Module
 const storeRouter = require("./routes/storeRouter")
 const hostRouter = require("./routes/hostRouter")
@@ -12,19 +14,28 @@ const rootDir = require("./utils/pathutil");
 const errorcontroller=require('./controllers/error')
 const { default: mongoose } = require('mongoose');
 const app = express();
-
+const DB_PATH='mongodb+srv://sharmarachit554_db_user:VQto3C7C1YTWVC1N@rachitdb.mxslh19.mongodb.net/airbnb?retryWrites=true&w=majority&appName=Rachitdb'
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(express.urlencoded({extended: true}));
-app.use(express.static(path.join(rootDir, 'public')))
-app.use(cookieParser());
-app.use((req, res, next) => {
-  req.isLoggedIn = req.cookies.isLoggedIn === 'true';
-  console.log('Parsed isLoggedIn:', req.isLoggedIn);
-  next();
+const store = new MongoDBStore({
+  uri: DB_PATH,
+  collection: 'sessions'
 });
+app.use(express.static(path.join(rootDir, 'public')))
+app.use(session({
+  secret: "Rachitxed",
+  resave: false,
+  saveUninitialized: true,
+  store
+}));
+
+app.use((req, res, next) => {
+  req.isLoggedIn = req.session.isLoggedIn
+  next();
+})
 
 app.use(authRouter);
 app.use(storeRouter);
@@ -39,7 +50,7 @@ app.use("/host", hostRouter);
 app.use(errorcontroller.pageNotFound)
 
 const PORT=3000;
-const DB_PATH='mongodb+srv://sharmarachit554_db_user:VQto3C7C1YTWVC1N@rachitdb.mxslh19.mongodb.net/airbnb?retryWrites=true&w=majority&appName=Rachitdb'
+
 mongoose.connect(DB_PATH).then(()=>{
     console.log('mongoose connected')
   app.listen(PORT,()=>{
--- controllers/authController.js
+++ controllers/authController.js
@@ -7,6 +7,11 @@ exports.getLogin=(req,res,next)=>{
 }
 exports.postLogin=(req,res,next)=>{
   console.log(req.body)
-  res.cookie("isLoggedIn", true);
+   req.session.isLoggedIn = true;
   res.redirect("/");
 };
+exports.postLogout = (req, res, next) => {
+  req.session.destroy(() => {
+    res.redirect("/login");
+  })
+}
